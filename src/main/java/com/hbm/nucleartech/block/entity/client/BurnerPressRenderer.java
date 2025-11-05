09ecc234a1304028835b5457b240d6a62c3da5d0
Nadwey <nadweyyy@gmail.com>
2025-11-05 10:57:14 +0100

    breaking effect poc

--- src/main/java/com/hbm/nucleartech/block/entity/client/BurnerPressRenderer.java
+++ src/main/java/com/hbm/nucleartech/block/entity/client/BurnerPressRenderer.java
@@ -2,17 +2,29 @@ package com.hbm.nucleartech.block.entity.client;
 
 import com.hbm.nucleartech.block.entity.BurnerPressEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.SheetedDecalTextureGenerator;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import org.joml.Math;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.cache.object.GeoCube;
+import software.bernie.geckolib.cache.object.GeoQuad;
+import software.bernie.geckolib.cache.object.GeoVertex;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class BurnerPressRenderer extends GeoBlockRenderer<BurnerPressEntity> {
+
+    float time = 0;
 
     public BurnerPressRenderer(BlockEntityRendererProvider.Context context) {
         super(new BurnerPressModel());
@@ -31,6 +43,8 @@ public class BurnerPressRenderer extends GeoBlockRenderer<BurnerPressEntity> {
                            float red, float green, float blue, float alpha) {
 
         super.postRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+
+        time += partialTick;
 
         ItemStack displayStack = animatable.getDisplayStack();
         if (!displayStack.isEmpty()) {
@@ -52,6 +66,26 @@ public class BurnerPressRenderer extends GeoBlockRenderer<BurnerPressEntity> {
             );
 
             poseStack.popPose();
+        }
+    }
+
+    @Override
+    public void createVerticesOfQuad(GeoQuad quad, Matrix4f poseState, Vector3f normal, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        super.createVerticesOfQuad(quad, poseState, normal, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+
+        int k = (int)(time * 0.05) % 10;
+
+        Matrix3f normalMat = new Matrix3f();
+        poseState.normal(normalMat);
+
+        VertexConsumer vertexConsumer = new SheetedDecalTextureGenerator(Minecraft.getInstance().renderBuffers().crumblingBufferSource().getBuffer(ModelBakery.DESTROY_TYPES.get(k)), poseState, normalMat, 1.0F);
+
+        for (GeoVertex vertex : quad.vertices()) {
+            Vector3f position = vertex.position();
+            Vector4f vector4f = poseState.transform(new Vector4f(position.x(), position.y(), position.z(), 1.0f));
+
+            vertexConsumer.vertex(vector4f.x(), vector4f.y(), vector4f.z(), red, green, blue, alpha, vertex.texU(),
+                    vertex.texV(), packedOverlay, packedLight, normal.x(), normal.y(), normal.z());
         }
     }
 }
